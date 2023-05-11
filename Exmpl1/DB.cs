696b41a9aa4dcc4b0374610ea8418dea2d7cad35
User 5 <User 5@PC209-5>
2023-05-11 15:31:46 +0300

    Добавьте файлы проекта.

---
+++ Exmpl1/DB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.IO;
+
+namespace Exmpl1
+{
+    internal class DB
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=PC209-5\SQLEXPRESS;Initial Catalog=platnaya_poliklinika;Integrated Security=True");
+
+        public void OpenConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Open) 
+            {
+                con.Close();
+            }
+        }
+        public SqlConnection GetConnection()
+        {
+            return con;
+        }
+    }
+}
---
+++ Exmpl1/Form_Sotr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exmpl1
+{
+    public partial class Form_Sotr : Form
+    {
+        public Form_Sotr()
+        {
+            InitializeComponent();
+        }
+    }
+}
